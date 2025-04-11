1adc66b466be10bfaac395b2aac724c0080f9789
Sebastian Ramirez <ramvillseb@gmai.com>
2025-04-10 21:34:39 -0700

    "done"

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 def main():
-
+    z=1
     N = int(input('Enter the number N: '))
     result = []
-
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    N=N+1
+    for i in range(N):
+        print(z, end=" ")
+        z=z+z
+        i=i+1
 
     ########################################
     # Do not delete the return statement
