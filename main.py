95b1a8c0c1565301679a03481611f0dc3f9b3538
Sebastian Ramirez <ramvillseb@gmai.com>
2025-04-10 23:30:37 -0700

    done

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+import numbers
+
+from numpy import append
+
+
 def main():
     N = int(input('Enter the number N: '))
     result = []
     z=1
     i=0
-    N=N+1
-    for i in range(N):
-        result=z
-        print(result, end=" ")
-        z=z+z
-        i=i+1
+    
+    for i in range(N+1):
+        result.append(2 ** i)
 
-        
+    print(result)
     
     
 
